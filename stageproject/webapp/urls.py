ed90a416c570e6d615f52dda3e83f150d124014e
taha bayhan <112399497+thbn1@users.noreply.github.com>
2023-07-28 02:11:25 +0300

    1
    1

--- stageproject/webapp/urls.py
+++ stageproject/webapp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index),
     path('index', views.index),
@@ -34,3 +35,4 @@ from django.conf.urls.static import static
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+ 
